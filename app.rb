3dba9397f28c59edf785779ba4a63703026f79f0
AdamNewmanBooth <164922100+AdamNewmanBooth@users.noreply.github.com>
2024-04-22 01:43:31 +0000

    Commit3

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ end
 
 get("/wizard_add") do
   @first_num = params.fetch("first_number").to_f 
-  @second_num =  params.fetch("first_number").to_f 
+  @second_num =  params.fetch("second_number").to_f 
   @result = @first_num + @second_num
   erb(:add_result)
 end
@@ -21,8 +21,8 @@ get("/subtract") do
 end
 
 get("/wizard_subtract") do
-  @sub_first_num = params.fetch("sub_first_number").to_f
-  @sub_second_num = params.fetch("sub_second_number").to_f
+  @sub_first_num = params.fetch("third_number").to_f
+  @sub_second_num = params.fetch("fourth_number").to_f
 
   @sub_result = @sub_first_num - @sub_second_num
   erb(:sub_result)
@@ -32,12 +32,12 @@ get("/multiply") do
   erb(:mult_form)
 end
 
-get("/wizard multiply") do
+get("/wizard_multiply") do
 
-  @mult_first_num = params.fetch("sub_first_number").to_f
-  @multsecond_num = params.fetch("sub_second_number").to_f
+  @mult_first_num = params.fetch("fifth_number").to_f
+  @mult_second_num = params.fetch("sixth_number").to_f
 
-  @mult_result = @sub_first_num - @sub_second_num
+  @mult_result = @mult_first_num * @mult_second_num
 
   erb(:mult_result)
 end
@@ -47,5 +47,10 @@ get("/divide") do
 end
 
 get("/wizard_divide") do
+  @div_first_num = params.fetch("seventh_number").to_f
+  @div_second_num = params.fetch("eighth_number").to_f
+
+  @div_result = @div_first_num / @div_second_num
+
   erb(:div_result)
 end
